82c2a2c18d476f40f8aad5ddab34f9cb979b8f4d
Stephanie Phillips <steph@ladymacbeth.uk>
2018-10-30 19:23:03 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+var dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = 	'#be29ec'
+
+
+//left
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+})
+
+
+
+//right
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+ 
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+})
