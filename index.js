c2353d7c04cf271df52fa02baa2025544db0e357
Stephanie Phillips <steph@ladymacbeth.uk>
2018-10-30 19:39:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ document.addEventListener('keydown', function(e) {
 
 //right
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(rightNumbers, 10)
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
  
   if (right > 0) {
-    dodger.style.right = `${right - 10}px`
+    dodger.style.left = `${left + 10}px`
   }
 }
 
